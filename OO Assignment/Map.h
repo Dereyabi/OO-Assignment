dcdab3ef1992ff1824a86797a7f68174a959863a
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2019-11-22 19:06:13 +0000

    I NEED TO DO WORK

--- OO Assignment/Map.h
+++ OO Assignment/Map.h
@@ -5,17 +5,17 @@ class map
 {
 public:
 
-	void clear(TypeKey, TypeValue);	//clears contents of selected cell of table
-	void insert();	//insert 
-	void erase(TypeKey, TypeValue);
+	void clear(TypeKey, TypeValue);		//clears all elements 
+	void insert(TypeKey, TypeValue);	//insert a new element 
+	void erase(TypeKey, TypeValue);		//erase a specific element 
 
-	bool empty(TypeKey, TypeValue);
-	int size();
-	int maxSize();
+	bool empty(TypeKey, TypeValue);		//returns a boolean for if the cell is empty
+	int size();							//returns the size of the array
+	int maxSize();						//returns the max size the array can be 
 
-	int count();
-	TypeValue find();
-	void contains()
+	int count();						
+	TypeValue find();					//looks up a certain element and returns where it is on the array
+	void contains()						//looks up which elements contain whatever is passed through the function 
 
 
 	TypeValue get(TypeKey, TypeValue) {return TypeValue};
