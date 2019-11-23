330f16a39811b7420b5f3b2483cf3fd1471473d7
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2019-11-23 01:45:27 +0000

    I have a start, gg bois
    class and function definitions created

--- OO Assignment/Map.h
+++ OO Assignment/Map.h
@@ -1,26 +1,104 @@
 #pragma once
 
 template <typename TypeKey, typename TypeValue>
-class map
+class CMap
 {
 public:
 
-	void clear(TypeKey, TypeValue);		//clears all elements 
-	void insert(TypeKey, TypeValue);	//insert a new element 
-	void erase(TypeKey, TypeValue);		//erase a specific element 
+	CMap::CMap() 
+	{
 
-	bool empty(TypeKey, TypeValue);		//returns a boolean for if the cell is empty
-	int size();							//returns the size of the array
-	int maxSize();						//returns the max size the array can be 
+	}
 
-	int count();						
-	TypeValue find();					//looks up a certain element and returns where it is on the array
-	void contains()						//looks up which elements contain whatever is passed through the function 
+	~CMap::CMap() 
+	{
 
+	}
 
-	TypeValue get(TypeKey, TypeValue) {return TypeValue};
+	void Clear();				//clears all elements 
+	void Insert(TypeKey, TypeValue);		//insert a new element 
+	void Erase(TypeKey, TypeValue);		//erase a specific element 
 
+	bool Empty(TypeKey, TypeValue);		//returns a boolean for if the cell is empty
+	unsigned int Size();				//returns the size of the array
+	unsigned int MaxSize();				//returns the max size the array can be 
+
+	int Count();					//return number of elements matching specific key, list of 	
+	TypeValue Find(TypeKey, TypeValue);			//looks up a certain element and returns where it is on the array
+
+	void Contains();			//looks up which elements contain whatever is passed through the function 
+	//pass over a typevalue, returns array of typekeys that contain original typevalue
+
+	TypeValue get(TypeKey) {return TypeValue};
 
 private:
 
+	const int maxLengthTemp = 696969;
+	unsigned int mLength;
+	unsigned int mMaxLength;
+	int mCount;
+
+	TypeKey* mStart;
+	TypeKey* mEnd;
+
 };
+
+template <typename TypeKey, typename TypeValue>
+void CMap<TypeKey, TypeValue>::Clear()
+{
+
+	mLength = 0;
+}
+
+template <typename TypeKey, typename TypeValue>
+void CMap<TypeKey, TypeValue>::Insert(TypeKey, TypeValue)
+{
+	
+	mLength++;
+}
+
+template <typename TypeKey, typename TypeValue>
+void CMap<TypeKey, TypeValue>::Erase(TypeKey, TypeValue)
+{
+
+	mLength--;
+}
+
+template <typename TypeKey, typename TypeValue>
+bool CMap<TypeKey, TypeValue>::Empty(TypeKey, TypeValue)
+{
+	if (TypeValue == NULL)
+	{
+		return false
+	}
+}
+
+template <typename TypeKey, typename TypeValue>
+unsigned int CMap<TypeKey, TypeValue>::Size()
+{
+	return mLength;
+}
+
+template <typename TypeKey, typename TypeValue>
+unsigned int CMap<TypeKey, TypeValue>::MaxSize()
+{
+	return mMaxLength;
+}
+
+template <typename TypeKey, typename TypeValue>
+int CMap<TypeKey, TypeValue>::Count()
+{
+
+}
+
+template <typename TypeKey, typename TypeValue>
+TypeValue CMap<TypeKey, TypeValue>::Find(TypeKey, TypeValue)
+{
+
+}
+
+template <typename TypeKey, typename TypeValue>
+void CMap<TypeKey, TypeValue>::Contains()
+{
+
+}
