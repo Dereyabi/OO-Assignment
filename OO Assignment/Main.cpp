68b44dfd2f8912f5f4b15e3f26df8b276ef08e5d
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2019-11-24 06:30:01 +0000

    Array Created, some tests done

--- OO Assignment/Main.cpp
+++ OO Assignment/Main.cpp
@@ -1,5 +1,51 @@
+#include "Map.h"
+#include <iostream>
+using namespace std;
 
-void main( )
+void main()
 {
 
+	int userResponse;
+	int userInputKey;
+	float userInputValue;
+
+	CMap<int, float>* map = new CMap<int, float>;
+
+
+	if (!map->Empty())
+	{
+		cout << "map is not empty" << endl;
+	}
+	else
+	{
+		cout << "map is empty" << endl;
+	}
+
+	cin >> userInputKey;
+	cin >> userInputValue;
+
+	map->Insert(userInputKey, userInputValue);
+
+	if (!map->Empty())
+	{
+		cout << "map is not empty" << endl;
+	}
+	else
+	{
+		cout << "map is empty" << endl;
+	}
+
+	map->Size();
+
+	map->Contains();
+
+	cin >> userResponse;
+
+	map->LookUp(userResponse);
+
+	system("pause");
+
+	map->Clear();
+
+	system("pause");
 }
--- OO Assignment/Map.h
+++ OO Assignment/Map.h
@@ -1,5 +1,7 @@
 #pragma once
 
+const unsigned int mMaxLength = 696969;
+
 template <typename TypeKey, typename TypeValue>
 class CMap
 {
@@ -7,24 +9,23 @@ public:
 
 	CMap::CMap() 
 	{
-
-	}
-
-	~CMap::CMap() 
-	{
-
+		mLength = 0;
+		Key = new TypeKey[mMaxLength];
+		Value = new TypeValue[mMaxLength];
+		mStart = &Key[0];
 	}
 
 	void Clear();				//clears all elements 
 	void Insert(TypeKey, TypeValue);		//insert a new element 
 	void Erase(TypeKey, TypeValue);		//erase a specific element 
 
-	bool Empty(TypeKey, TypeValue);		//returns a boolean for if the cell is empty
+	bool Empty();		//returns a boolean for if the map is empty
 	unsigned int Size();				//returns the size of the array
 	unsigned int MaxSize();				//returns the max size the array can be 
 
-	int Count();					//return number of elements matching specific key, list of 	
+	int Count();					//return number of elements matching specific key. this is useless, 1 key to 1 value
 	TypeValue Find(TypeKey, TypeValue);			//looks up a certain element and returns where it is on the array
+	void LookUp(TypeKey);
 
 	void Contains();			//looks up which elements contain whatever is passed through the function 
 	//pass over a typevalue, returns array of typekeys that contain original typevalue
@@ -33,10 +34,12 @@ public:
 
 private:
 
-	const int maxLengthTemp = 696969;
 	unsigned int mLength;
-	unsigned int mMaxLength;
+
 	int mCount;
+
+	TypeKey* Key;
+	TypeValue* Value;
 
 	TypeKey* mStart;
 	TypeKey* mEnd;
@@ -46,59 +49,102 @@ private:
 template <typename TypeKey, typename TypeValue>
 void CMap<TypeKey, TypeValue>::Clear()
 {
+	//need iterator through the array to delete everything in it, return the length of the array to 0
+	for (int i = 0; i < mLength; i++)
+	{
+		//delete whats in the key array and the value array
 
+	}
+	//set the length of the array to 0 as everything has been deleted
 	mLength = 0;
 }
 
 template <typename TypeKey, typename TypeValue>
-void CMap<TypeKey, TypeValue>::Insert(TypeKey, TypeValue)
+void CMap<TypeKey, TypeValue>::Insert(TypeKey k, TypeValue v)
 {
-	
+	//takes a key and a value, inserts key into key array and value into value array 
+	Key[mLength] = k;
+	Value[mLength] = v;
 	mLength++;
 }
 
 template <typename TypeKey, typename TypeValue>
-void CMap<TypeKey, TypeValue>::Erase(TypeKey, TypeValue)
+void CMap<TypeKey, TypeValue>::Erase(TypeKey k, TypeValue v)
 {
-
+	//takes a key, erase the key and the value, reduce length by 1
+	for (int i = 0; i < mLength; i++)
+	{
+		if (Key[i] == k)
+		{
+			//erase key and value associated with position, make length 1 less, move everything ahead to being 1 position less on the array
+		}
+	}
 	mLength--;
 }
 
 template <typename TypeKey, typename TypeValue>
-bool CMap<TypeKey, TypeValue>::Empty(TypeKey, TypeValue)
+bool CMap<TypeKey, TypeValue>::Empty()
 {
-	if (TypeValue == NULL)
+	//checks if the map is empty or not
+
+	if (mLength == 0)
 	{
-		return false
+		return true;
+	}
+	else
+	{
+		return false;
 	}
 }
 
 template <typename TypeKey, typename TypeValue>
 unsigned int CMap<TypeKey, TypeValue>::Size()
 {
+	//just returns what the current length of the array is 
+	cout << mLength << endl;
 	return mLength;
 }
 
 template <typename TypeKey, typename TypeValue>
 unsigned int CMap<TypeKey, TypeValue>::MaxSize()
 {
+	//just returns what the overall length of the array is
+	cout << mMaxLength << endl;
 	return mMaxLength;
 }
 
 template <typename TypeKey, typename TypeValue>
 int CMap<TypeKey, TypeValue>::Count()
 {
-
+	//useless lul
 }
 
 template <typename TypeKey, typename TypeValue>
-TypeValue CMap<TypeKey, TypeValue>::Find(TypeKey, TypeValue)
+TypeValue CMap<TypeKey, TypeValue>::Find(TypeKey k, TypeValue v)
 {
-
+	//
 }
 
 template <typename TypeKey, typename TypeValue>
 void CMap<TypeKey, TypeValue>::Contains()
 {
 
+}
+
+template <typename TypeKey, typename TypeValue>
+void CMap<TypeKey, TypeValue>::LookUp(TypeKey k)
+{
+	for (int i = 0; i < mLength; i++)
+	{
+		if (Key[i] == k)
+		{
+			cout << "the key is: " << Key[i] << endl;
+			cout << "the value is:" << Value[i] << endl;
+		}
+		else
+		{
+			cout << "Key not found" << endl;
+		}
+
+	}
 }
